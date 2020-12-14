d8a7bd75335d907c180137e166790284895b5d33
Jack Hubert <jphubert93@gmail.com>
2020-12-14 14:47:37 -0700

    commit tree 96d398e22524ccb4532d16f9388bac8319cf5873

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+
+    def initialize
+        # some code
+    end
+
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+    
+    def initialize
+        # some code
+    end
+
+    def talk
+        "Hello World!"
+    end
+
+    def walk
+        "The Person is walking"
+    end
+
+end
